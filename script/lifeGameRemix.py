961740c722c4365b8f7d34d97c1f67e3bc16eadd
J. Jesús González Mairena <jiuxej@gmail.com>
2022-04-26 19:54:52 +0200

    Corregido fallo en contador de vecinos. Modificado valor de celulas buenas.

--- script/lifeGameRemix.py
+++ script/lifeGameRemix.py
@@ -19,6 +19,10 @@ dimCH = height / nyC
 
 gameState = np.zeros((nxC, nyC))
 
+n_neigh_good = 0
+n_neigh_bad = 0
+n_neigh_killer = 0
+
 # Generar moviles
 for num_mov in range(0,40):
     pivot = random.randint(2,48)
@@ -38,18 +42,19 @@ while True:
     for y in range(0,nxC):
         for x in range(0,nyC):
 
-            if gameState[x,y] == 2 or gameState[x,y] == 4:
+            if gameState[x,y] == 3 or gameState[x,y] == 4:
                 n_neigh_good = 0
                 n_neigh_bad = 0
                 n_neigh_killer = 0
                 for i in range(-1,1):
                     for j in range(-1,1):
-                        if gameState[(x + i) % nxC, (y + j) % nyC] == 2:
-                            n_neigh_good += 1
-                        elif gameState[(x + i) % nxC, (y + j) % nyC] == 4:
-                            n_neigh_bad += 1
-                        elif gameState[(x + i) % nxC, (y + j) % nyC] == 6:
-                            n_neigh_killer += 1
+                        if not (i == 0 and j == 0):
+                            if gameState[(x + i) % nxC, (y + j) % nyC] == 3:
+                                n_neigh_good += 1
+                            elif gameState[(x + i) % nxC, (y + j) % nyC] == 4:
+                                n_neigh_bad += 1
+                            elif gameState[(x + i) % nxC, (y + j) % nyC] == 6:
+                                n_neigh_killer += 1
 
             n_neigh = (gameState[(x - 1) % nxC, (y - 1) % nyC]) % 2 + \
                       (gameState[(x) % nxC, (y - 1) % nyC]) % 2 + \
@@ -70,10 +75,10 @@ while True:
 
             # Regla 3: Una celula viva con 5 vecinos vivos, nace como una celula buena o mala
             if gameState[x, y] == 1 and n_neigh == 5:
-                newGameState[x, y] = random.choice([2, 4])
+                newGameState[x, y] = random.choice([3, 4])
 
             # Regla 4: Una celula buena vecina de 2 o mas celulas malas, muere
-            if gameState[x, y] == 2 and n_neigh_bad > 2:
+            if gameState[x, y] == 3 and n_neigh_bad > 2:
                 newGameState[x, y] = 0
 
             # Regla 5: Una celula mala vecina de una celula mala puede morir o convertirse en asesina
@@ -81,7 +86,7 @@ while True:
                 newGameState[x, y] = random.choice([0, 6])
 
             # Regla 6: Una celula nacida vecina de una celula asesina, muere
-            if (gameState[x,y] == 2 or gameState[x,y] == 4) and n_neigh_killer != 0:
+            if gameState[x,y] == 3 and n_neigh_killer != 0:
                 newGameState[x, y] = 0
 
             poly = [((x) * dimCW, y * dimCH),
@@ -91,7 +96,7 @@ while True:
 
             if newGameState[x, y] == 0:
                 pygame.draw.polygon(screen,(128, 128, 128), poly, 1)
-            elif newGameState[x, y] == 2:
+            elif newGameState[x, y] == 3:
                 pygame.draw.polygon(screen,(0, 255, 0), poly, 0)
             elif newGameState[x, y] == 4:
                 pygame.draw.polygon(screen,(255, 0, 0), poly, 0)
